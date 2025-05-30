23606edfd4126dc37f99efeb75abaf27238fff26
soariix <sarinhapachecosoares@gmail.com>
2025-05-30 18:29:04 -0300

    feat: adiciona novos campos ao ReservaResponseDTO para melhorar resposta da API

--- src/main/java/com/salafacil/SalaFacilSpace/dto/ReservaResponseDTO.java
+++ src/main/java/com/salafacil/SalaFacilSpace/dto/ReservaResponseDTO.java
@@ -14,11 +14,15 @@ public class ReservaResponseDTO {
     private String nomeProfessor;
     private String data;
     private String horario;
+    private String nomeCurso;
+    private String nomeLaboratorio;
 
     public ReservaResponseDTO(Reserva reserva) {
         this.id = reserva.getId();
         this.nomeProfessor = reserva.getProfessor().getNome();
         this.data = reserva.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         this.horario = reserva.getHorario().toString();
+        this.nomeCurso = reserva.getCurso().getNome();
+        this.nomeLaboratorio = reserva.getLaboratorio().getNome();
     }
 }
